e7b1d9389dea89636ce0f7a746a7bb76010014fc
CHANG FU CHIAO <zector1030@gmail.com>
2017-05-08 12:41:20 +0800

    connect api

---
+++ answers/hw4/index.js
@@ -0,0 +1,29 @@
+$.ajax({
+  type: "GET",
+  url: "https://api.twitch.tv/kraken/streams/",
+  headers:{'Client-ID':'8okp5kcn7dr13mazyq5anbwqvnlclp'},
+//  data: {'game':'League%20of%20Legends', 'limit':20},
+  data:{'limit':20},
+  success: function(data, textStatus, jqXHR) {
+    console.log(data.streams);
+    $(".col").each(function(index) {
+      if (index < data.streams.length) {
+        console.log(data.streams[index].channel.status);
+        console.log(data.streams[index].channel.display_name);
+        console.log(data.streams[index].channel.logo);
+        jQuery(".preview img", this).attr('src',data.streams[index].preview.medium);
+        jQuery(".bottom .avatar img", this).attr('src',data.streams[index].channel.logo);
+        jQuery(".bottom .intro .channel_name", this).text(data.streams[index].channel.status);
+        jQuery(".bottom .intro .owner_name", this).text(data.streams[index].channel.display_name);
+        jQuery(".bottom", this).css('dispaly','block');
+        jQuery(".preview", this).css('dispaly','block');
+      }
+      else {
+        jQuery(".bottom", this).hide();
+        jQuery(".preview", this).hide();
+      }
+    })
+  },
+  dataType:'json'
+});
+
