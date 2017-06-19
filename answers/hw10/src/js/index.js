592ffb0116bd3d6ed0cdbb5d3fae542f66cefc46
CHANG FU CHIAO <zector1030@gmail.com>
2017-06-19 15:56:36 +0800

    fix lint error

--- answers/hw10/src/js/index.js
+++ answers/hw10/src/js/index.js
@@ -1,32 +1,33 @@
-var utils = require('./utils.js');
+const utils = require('./utils.js');
+
 function throttle(fn, wait) {
-    console.log('test');
-    var time = Date.now();
-    return function() {
-        if(document.body.scrollTop + document.documentElement.clientHeight > document.documentElement.offsetHeight - 300) {
-            if ((time + wait - Date.now()) < 0) {
-                utils.add_stream_count(20);
-                fn(false);
-                time = Date.now();
-            }
-        }
-    };
+  let time = Date.now();
+  return () => {
+    if (document.body.scrollTop + document.documentElement.clientHeight
+        > document.documentElement.offsetHeight - 300) {
+      if (((time + wait) - Date.now()) < 0) {
+        utils.add_stream_count(20);
+        fn(false);
+        time = Date.now();
+      }
+    }
+  };
 }
 
-function load_streams_info() {
-  var lang = utils.get_lang();
-  var offset = utils.get_stream_count();
-  var r = new XMLHttpRequest();
-  var params = 'game='+encodeURIComponent('League of Legends')+`&limit=20&offset=${offset}&language=${lang}`;
-  r.open("GET", `https://api.twitch.tv/kraken/streams/?${params}`, true);
-  r.onload = function () {
-    console.log(r.response);
-    for (var stream of r.response.streams) {
-      console.log(stream.preview.medium);
-      console.log(stream.channel.logo);
-      console.log(stream.channel.status);
-      console.log(stream.channel.display_name);
-      var newNode = document.createElement('div');
+function loadStreamsInfo() {
+  const lang = utils.get_lang();
+  const offset = utils.get_stream_count();
+  const r = new XMLHttpRequest();
+  const params = `game=${encodeURIComponent('League of Legends')}&limit=20&offset=${offset}&language=${lang}`;
+  r.open('GET', `https://api.twitch.tv/kraken/streams/?${params}`, true);
+  r.onload = () => {
+    // console.log(r.response);
+    r.response.streams.forEach((stream) => {
+      // console.log(stream.preview.medium);
+      // console.log(stream.channel.logo);
+      // console.log(stream.channel.status);
+      // console.log(stream.channel.display_name);
+      const newNode = document.createElement('div');
       newNode.innerHTML = `
       <div class="col">
         <div class="preview"><img src=${stream.preview.medium} /></div>
@@ -38,42 +39,41 @@ function load_streams_info() {
           </div>
         </div>
       </div>`;
-      var parentDiv = document.getElementById("insert-before-this").parentNode;
-      var sp2 = document.getElementById("insert-before-this");
+      const parentDiv = document.getElementById('insert-before-this').parentNode;
+      const sp2 = document.getElementById('insert-before-this');
       parentDiv.insertBefore(newNode, sp2);
-    }
+    });
   };
   r.responseType = 'json';
   r.setRequestHeader('Client-ID', '8okp5kcn7dr13mazyq5anbwqvnlclp');
   r.send(params);
 }
 
-function reload_lang_item() {
-  var lang = utils.get_lang();
-  document.getElementsByClassName("title")[0].innerHTML = utils.get_title();
-  reload_cols();
-}
-
-function reload_cols() {
-  while (true) {
-    var removed = document.getElementById('insert-before-this').previousSibling;
-    if (removed == null) {break;}
+function reloadCols() {
+  for (;;) {
+    const removed = document.getElementById('insert-before-this').previousSibling;
+    if (removed == null) { break; }
     removed.remove();
   }
   utils.reset_stream_count();
-  load_streams_info();
+  loadStreamsInfo();
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function reloadLangItem() {
+  document.getElementsByClassName('title')[0].innerHTML = utils.get_title();
+  reloadCols();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   utils.set_lang('zh-tw');
-  reload_lang_item();
-  window.addEventListener('scroll', throttle(load_streams_info, 1000));
-  document.getElementById('set_en').addEventListener('click', function(){
-    utils.set_lang('en')
-    reload_lang_item();
+  reloadLangItem();
+  window.addEventListener('scroll', throttle(loadStreamsInfo, 1000));
+  document.getElementById('set_en').addEventListener('click', () => {
+    utils.set_lang('en');
+    reloadLangItem();
   });
-  document.getElementById('set_zhtw').addEventListener('click', function(){
-    utils.set_lang('zh-tw')
-    reload_lang_item();
+  document.getElementById('set_zhtw').addEventListener('click', () => {
+    utils.set_lang('zh-tw');
+    reloadLangItem();
   });
 });
