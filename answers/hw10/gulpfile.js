e37515eef09ff4b9e918de2ebfc73f87e8a7e983
CHANG FU CHIAO <zector1030@gmail.com>
2017-06-19 14:25:01 +0800

    fix lint error

--- answers/hw10/gulpfile.js
+++ answers/hw10/gulpfile.js
@@ -1,42 +1,42 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var uglifycss = require('gulp-uglifycss');
-var babel = require('gulp-babel');
-var jshint = require('gulp-jshint');
-var stylus = require('gulp-stylus');
-var webpack = require('gulp-webpack');
-var fs = require('fs');
-var replace = require('gulp-replace');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+// const concat = require('gulp-concat');
+// const uglifycss = require('gulp-uglifycss');
+const babel = require('gulp-babel');
+const jshint = require('gulp-jshint');
+const stylus = require('gulp-stylus');
+// const webpack = require('gulp-webpack');
+const fs = require('fs');
+const replace = require('gulp-replace');
+
 gulp.task('default', ['html']);
 gulp.task('build', ['js', 'css']);
-gulp.task('lint', function() {
-    gulp.src('src/js/*.js')
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-gulp.task('js', function(){
-    return gulp.src('src/js/bundle.js')
-//        .pipe(concat('bundle.js'))
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/'));
-});
-gulp.task('css', function () {
-    return gulp.src('src/css/*.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('build/'));
-});
-gulp.task('html', function () {
-    return gulp.src('src/*.html')
-    .pipe(replace(/<style><\/style>/g, function(s){
-        let style = fs.readFileSync('./build/style.css', 'utf8');
-        return `<style>${style}</style>`;
+gulp.task('lint', (() => {
+  gulp.src('src/js/*.js')
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+}));
+gulp.task('js', (() => {
+  gulp.src('src/js/bundle.js')
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/'));
+}));
+gulp.task('css', (() => {
+  gulp.src('src/css/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('build/'));
+}));
+gulp.task('html', (() => {
+  gulp.src('src/*.html')
+    .pipe(replace(/<style><\/style>/g, () => {
+      const style = fs.readFileSync('./build/style.css', 'utf8');
+      return `<style>${style}</style>`;
     }))
-    .pipe(replace(/<script><\/script>/g, function(s){
-        let js = fs.readFileSync('./build/bundle.js', 'utf8');
-        return `<script>${js}</script>`;
+    .pipe(replace(/<script><\/script>/g, () => {
+      const js = fs.readFileSync('./build/bundle.js', 'utf8');
+      return `<script>${js}</script>`;
     }))
     .pipe(gulp.dest('dist/'));
-});
+}));
